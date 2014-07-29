6b19083e4afb06a6956598fa69ac02d6ecf10186
sandeepchhapola <schhapola0001@gmail.com>
2014-07-29 20:38:29 +0530

    initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ninjaWeatherApiApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
