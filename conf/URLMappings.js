6ef445afa95d07b0a4c48a7cad21f71a45a5d7c4
sandeepchhapola <schhapola0001@gmail.com>
2014-08-03 19:28:02 +0530

    twitter logIn implemented

--- conf/URLMappings.js
+++ conf/URLMappings.js
@@ -6,50 +6,75 @@
 
 //Require Controllers
 var controllers = {
-  cluster: require("../controllers/ClusterController"),
-  user: require("../controllers/UserController"),
-  home: require("../controllers/HomeController")
+    cluster: require("../controllers/ClusterController"),
+    user: require("../controllers/UserController"),
+    home: require("../controllers/HomeController")
 };
 
 var passport = require('passport')
-  , FacebookStrategy = require('passport-facebook').Strategy;
+    , FacebookStrategy = require('passport-facebook').Strategy
+    ,TwitterStrategy = require('passport-twitter').Strategy;
+
 
 passport.serializeUser(function (user, done) {
-  done(null, user);
+    done(null, user);
 });
 
 passport.deserializeUser(function (user, done) {
-  done(null, user);
+    done(null, user);
 });
 
 passport.use(new FacebookStrategy({
-    clientID: _config.facebookAuth.clientID,
-    clientSecret: _config.facebookAuth.clientSecret,
-    callbackURL: _config.facebookAuth.callbackURL
-  },
-  function (accessToken, refreshToken, profile, done) {
+        clientID: _config.facebookAuth.clientID,
+        clientSecret: _config.facebookAuth.clientSecret,
+        callbackURL: _config.facebookAuth.callbackURL
+    },function (accessToken, refreshToken, profile, done) {
 
-    done(null, profile);
-  }
+        done(null, profile);
+    }
 ));
+
+passport.use(new TwitterStrategy({
+    consumerKey: _config.twitterAuth.consumerKey,
+    consumerSecret: _config.twitterAuth.consumerSecret,
+    callbackURL: _config.twitterAuth.callbackURL
+}, function (token, tokenSecret, profile, done) {
+    done(null, profile);
+}));
+
 //Cluster API
 _app.get("/cluster/worker/list", controllers.cluster.list);
 
 //Home/Auth URL Mappings
 _app.get('/', controllers.home.index);
 
-//User routes
-_app.get('/logout', controllers.user.logout);
 
 // Redirect the user to Facebook for authentication.  When complete,
 // Facebook will redirect the user back to the application at
 //     /auth/facebook/callback
-_app.get('/auth/facebook', passport.authenticate('facebook', {scope:'email'}));
+_app.get('/auth/facebook', passport.authenticate('facebook', {scope: 'email'}));
 
 // Facebook will redirect the user to this URL after approval.  Finish the
 // authentication process by attempting to obtain an access token.  If
 // access was granted, the user will be logged in.  Otherwise,
 // authentication has failed.
 _app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { successRedirect: '/',
-    failureRedirect: '/login' }));
+    passport.authenticate('facebook', { successRedirect: '/',
+        failureRedirect: '/login' }));
+
+
+// Redirect the user to Twitter for authentication.  When complete, Twitter
+// will redirect the user back to the application at
+//   /auth/twitter/callback
+_app.get('/auth/twitter', passport.authenticate('twitter'));
+
+// Twitter will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+_app.get('/auth/twitter/callback',
+    passport.authenticate('twitter', { successRedirect: '/',
+        failureRedirect: '/login' }));
+
+//User routes
+_app.get('/logout', controllers.user.logout);
