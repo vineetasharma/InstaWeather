bbbdbe8f5d3bf48a8a5f18a7a5ccc477a34f6ea0
sandeepchhapola <schhapola0001@gmail.com>
2014-08-03 17:13:34 +0530

    profileCtrl controller skeleton added

---
+++ web-app/dev/scripts/controllers/profile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yoApp.controller:profileCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yoApp
+ */
+angular.module('yoApp')
+    .controller('profileCtrl',['$scope',function ($scope) {
+        console.log(">>>>>>>>>>>main page called>>>>>>>>>>>");
+    }]);
