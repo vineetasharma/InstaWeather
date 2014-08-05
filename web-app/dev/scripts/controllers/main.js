72d5663a5d87631bd01e12cff16e3e6b89ce77da
sandeepchhapola <schhapola0001@gmail.com>
2014-08-05 18:53:33 +0530

    geoNames Api call implemented

--- web-app/dev/scripts/controllers/main.js
+++ web-app/dev/scripts/controllers/main.js
@@ -10,4 +10,21 @@
 angular.module('yoApp')
     .controller('mainCtrl',['$scope',function ($scope) {
         console.log(">>>>>>>>>>>main page called>>>>>>>>>>>");
+        jQuery.ajax({
+            url: "http://api.geonames.org/searchJSON?",
+            dataType: 'jsonp',
+            data: {
+                featureClass: "P",
+                style: "full",
+                maxRows: 12,
+                name_startsWith: $('#city').val(),
+                username:'vineetasharma'
+            },
+            success: function( data ) {
+                //Display city name, state name, country name
+                for(var d in data.geonames){
+                    console.log(data.geonames[d].countryName);
+                }
+            }
+        });
     }]);
