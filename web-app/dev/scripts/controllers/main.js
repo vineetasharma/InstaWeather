e71e6c48d6cb8dc1894af8e0d3ae04b0a5cf9314
vineetasharma <vineeta.sharma@intelligrape.com>
2014-08-05 17:20:37 +0530

    implement search location autocomplete

--- web-app/dev/scripts/controllers/main.js
+++ web-app/dev/scripts/controllers/main.js
@@ -9,5 +9,16 @@
  */
 angular.module('yoApp')
     .controller('mainCtrl',['$scope',function ($scope) {
+
         console.log(">>>>>>>>>>>main page called>>>>>>>>>>>");
+        jQuery(function(){
+
+            var options = {
+                //map: ".map_canvas",
+                location: "NYC"
+            };
+console.log('in jquery function');
+            $("#city").geocomplete(options);
+
+        });
     }]);
