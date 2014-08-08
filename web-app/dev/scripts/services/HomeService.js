166655e7674ff325f2b81425c78977f06687aabd
sandeepchhapola <schhapola0001@gmail.com>
2014-08-08 11:00:29 +0530

    Retrieving current weather data from geoName Api done

--- web-app/dev/scripts/services/HomeService.js
+++ web-app/dev/scripts/services/HomeService.js
@@ -1,13 +1,39 @@
 angular.module('yoApp')
-    .service('HomeService', ['$http', function ($http) {
-        var baseUrl = "http://localhost:9092";
-        this.isLoggedIn = function () {
-            $http({method: 'GET', url: baseUrl + '/isLoggedIn'}).
-                success(function (value) {
-                    return value;
-                }).
-                error(function (err) {
-                    throw err;
+    .service('HomeService', function () {
+       this.getDeatails=function(callback) {
+            jQuery.ajax({
+                url: "http://ws.geonames.org/searchJSON",
+                dataType: "jsonp",
+                data: {
+                    featureClass: "P",
+                    style: "full",
+                    name_equals: (jQuery("#city").val().toString().split(','))[0],
+                    username: 'vineetasharma'
+                },
+                success: function (data) {
+                    callback(data);
+                }
+               ,error: function(err) {
+                    callback(null);
+                }
             });
         };
-    }]);
+        this.getWeatherDeatails=function(result) {
+            jQuery.ajax({
+                url: "http://api.geonames.org/findNearByWeatherJSON?",
+                dataType: "jsonp",
+                data: {
+                    lat:result.geonames[0].lat,
+                    lng:result.geonames[0].lng,
+                    username: 'vineetasharma'
+                },
+                success: function (data) {
+                    console.log(result);
+                    console.log(data);
+                }
+                , error: function (err) {
+                    console.log(err);
+                }
+            });
+        }
+    });
