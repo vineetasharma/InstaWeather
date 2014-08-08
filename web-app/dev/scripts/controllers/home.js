51ba4227863179e55be5ea7508507c6fde5f134e
vineetasharma <vineeta.sharma@intelligrape.com>
2014-08-08 15:43:57 +0530

    selected location weather details show on home page

--- web-app/dev/scripts/controllers/home.js
+++ web-app/dev/scripts/controllers/home.js
@@ -2,10 +2,39 @@
  * Created by sandeepchhapola on 30/7/14.
  */
 angular.module('yoApp')
-    .controller('homeCtrl',[ '$scope',function ($scope) {
+    .controller('homeCtrl',['$scope','HomeService',function ($scope,HomeService) {
+        jQuery(function () {
+            var options = {
+                types: ['(cities)']
+            };
+            $("#city").geocomplete(options);
+        });
+
         $scope.signIn = function () {
             jQuery('#signin').modal({
                 keyboard: true
             });
         };
+
+        $scope.WILocalionResult;
+        $scope.WIWeatherResult;
+
+        $scope.getDetails=function(){
+            HomeService.getDeatails(function(result) {
+                $scope.WILocalionResult=result;
+                HomeService.getWeatherDeatails(result,function(weatherInfo){
+                    $scope.WIWeatherResult=weatherInfo;
+                    console.log($scope.WIWeatherResult);
+                    $scope.$apply();
+                });
+
+               // console.log($scope.WILocalionResult,"wether information");
+            });
+        }
+        $scope.imageURL='http://past.theweathernetwork.com/common/images/web/wicons/d.gif';/*
+        'http://past.theweathernetwork.com/common/images/web/wicons/a.gif' sunny
+        'http://past.theweathernetwork.com/common/images/web/wicons/e.gif' variable
+        'http://past.theweathernetwork.com/common/images/web/wicons/f.gif' cloudy with sunny breaks
+
+        'http://past.theweathernetwork.com/common/images/web/wicons/k.gif' cloudy*/
     }]);
--- web-app/dev/scripts/services/HomeService.js
+++ web-app/dev/scripts/services/HomeService.js
@@ -1,13 +1,49 @@
 angular.module('yoApp')
-    .service('HomeService', ['$http', function ($http) {
-        var baseUrl = "http://localhost:9092";
-        this.isLoggedIn = function () {
-            $http({method: 'GET', url: baseUrl + '/isLoggedIn'}).
-                success(function (value) {
-                    return value;
-                }).
-                error(function (err) {
-                    throw err;
+    .service('HomeService', function () {
+       this.getDeatails=function(callback) {
+            jQuery.ajax({
+                url: "http://ws.geonames.org/searchJSON",
+                dataType: "jsonp",
+                data: {
+                    featureClass: "P",
+                    style: "full",
+                    name_equals: (jQuery("#city").val().toString().split(','))[0],
+                    username: 'vineetasharma'
+                },
+                success: function (data) {
+                    $http.post("/addlocationdata",data)
+                        .success(function () {
+                            console.log("Information sucessfully added");
+                        }).
+                        error(function (error) {
+                            console.log("error during saving information: ",error.message);
+                        });
+                    console.log(data,"alwar info");
+                    callback(data);
+                }
+               ,error: function(err) {
+                    callback(null);
+                }
             });
         };
-    }]);
+        this.getWeatherDeatails=function(result,callback) {
+            jQuery.ajax({
+                url: "http://api.geonames.org/findNearByWeatherJSON?",
+                dataType: "jsonp",
+                data: {
+                    lat:result.geonames[0].lat,
+                    lng:result.geonames[0].lng,
+                    username: 'vineetasharma'
+                },
+                success: function (data) {
+                    console.log(result);
+                    console.log(data);
+                   callback(data);
+                }
+                , error: function (err) {
+                    console.log(err);
+                    callback(null);
+                }
+            });
+        }
+    });
