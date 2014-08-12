a9ad84ac80185f0a53a5b270affaed275cf85392
sandeepchhapola <schhapola0001@gmail.com>
2014-08-12 13:10:14 +0530

    bug in getting weather information is resolved

--- web-app/dev/scripts/controllers/home.js
+++ web-app/dev/scripts/controllers/home.js
@@ -2,7 +2,7 @@
  * Created by sandeepchhapola on 30/7/14.
  */
 angular.module('yoApp')
-    .controller('homeCtrl',['$scope','HomeService',function ($scope,HomeService) {
+    .controller('homeCtrl', ['$scope', 'HomeService', function ($scope, HomeService) {
         jQuery(function () {
             var options = {
                 types: ['(cities)']
@@ -10,16 +10,17 @@ angular.module('yoApp')
             $("#city").geocomplete(options);
         });
         /*disabled a button*/
-        $(document).ready(function(){
-            $('button').attr('disabled','disabled');
+        $(document).ready(function () {
+            $('button').attr('disabled', 'disabled');
             $('#info').hide();
-            $('input[type="text"]').change(function(){
-                if($(this).val != ''){
+            $('#city').change(function () {
+                if ($(this).val != '') {
                     $('button').removeAttr('disabled');
                 }
             });
         });
 
+        /*login pop up*/
         $scope.signIn = function () {
             jQuery('#signin').modal({
                 keyboard: true
@@ -29,23 +30,28 @@ angular.module('yoApp')
         $scope.WILocalionResult;
         $scope.WIWeatherResult;
 
+        /*recieving location information and then weather information to show weather information on home page*/
+
         $scope.getDetails = function () {
-            $('button').attr('disabled','disabled');
-            HomeService.getDeatails(function (result) {
-                $scope.WILocalionResult = result;
-                HomeService.getWeatherDeatails(result, function (weatherInfo) {
-                    console.log(weatherInfo);
-                    if(!(weatherInfo.weatherObservation)) {
-                        $('#info').show();
-                        $('#info').html("Sorry! weather information not found");
-                    }
-                    else
-                        $('#info').hide();
+            $scope.WILocalionResult=null;
+            $scope.WIWeatherResult=null;
+            $('button').attr('disabled', 'disabled');
+            $('#info').hide();
+            HomeService.getDetails(function (result) {
+                if (result) {
+                    $scope.WILocalionResult = result;
+                    HomeService.getWeatherDetails(result, function (weatherInfo) {
+                        console.log(weatherInfo,'wether info in homejs');
+                        $scope.WIWeatherResult = weatherInfo;
 
-                    $scope.WIWeatherResult = weatherInfo;
-                    $scope.$apply();
-                });
+                        $scope.$apply();
+                    });
 
+                }
+                else {
+                    $('#info').show();
+                    $('#info').html("Sorry! weather information not found");
+                }
             });
         }
     }]);
