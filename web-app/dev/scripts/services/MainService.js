ae0754a175558095ccef6d979d7196775bc48068
sandeepchhapola <schhapola0001@gmail.com>
2014-08-17 10:35:48 +0530

    alert message on success or error is added

--- web-app/dev/scripts/services/MainService.js
+++ web-app/dev/scripts/services/MainService.js
@@ -3,9 +3,12 @@ angular.module('yoApp')
         this.addEmail=function(userId,email){
             $http.put('/addEmail/'+userId,{email:email}).success(function (data) {
                 console.log("email successfully added:",data);
-                    jQuery('#promptemail').removeClass('in');
+                jQuery('#promptemail').toggle();
+                jQuery('.modal-backdrop').toggle();
+                $(".alert-success").removeClass("in").show().delay(200).addClass("in").fadeOut(5000);
             }).error(function (err) {
                 console.log("Error in adding email:",err);
+                $(".alert-error").removeClass("in").show().delay(200).addClass("in").fadeOut(5000);
             });
         };
     }]);
