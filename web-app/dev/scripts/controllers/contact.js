73abe6b10d5429dc403df91d8f6c4fab6b2978b8
vineetasharma <vineeta.sharma@intelligrape.com>
2014-08-18 13:50:19 +0530

    loader added

--- web-app/dev/scripts/controllers/contact.js
+++ web-app/dev/scripts/controllers/contact.js
@@ -1,6 +1,9 @@
 angular.module('yoApp')
     .controller('contactCtrl', ['$scope', 'ContactService', function ($scope, ContactService) {
         $scope.sendMail = function () {
+            $scope.loader=true;
+
+
             $scope.disable = true;
             if ($scope.email && $scope.name && $scope.message) {
                 ContactService.isValidEmail($scope.email, function (valid) {
@@ -19,18 +22,22 @@ angular.module('yoApp')
                             message: $scope.message
                         };
                         ContactService.sendMail(mailData, function (data) {
-                            /*var progressBarWidth =10*$(".container-progress-bar").width()/ 100;
-                            $(".progressbar").width(progressBarWidth).html(10 + "% ");*/
+
                             if (data) {
                                 $(".alert-success").removeClass("in").show().delay(200).addClass("in").fadeOut(5000);
+                                $(".progressbar").width($(".progress").width()).html(100 + "% ");
                                 $scope.name = '';
                                 $scope.email = '';
                                 $scope.message = '';
                                 $scope.disable = false;
+                                $scope.loader=false;
+
                             }
                             else {
                                 $scope.disable = false;
                                 $(".alert-error").removeClass("in").show().delay(200).addClass("in").fadeOut(5000);
+                                $scope.loader=false;
+
                             }
                         });
 
@@ -40,6 +47,7 @@ angular.module('yoApp')
             }
             else{
                 $scope.disable = false;
+                $scope.loader=false;
                 $(".alert-dismissable").removeClass("in").show().delay(200).addClass("in").fadeOut(5000);
 
             }
