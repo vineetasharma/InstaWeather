877ab4cdc022b9f00703693452d6fcf5cb78f2dc
vineetasharma <vineeta.sharma@intelligrape.com>
2014-08-19 15:41:38 +0530

    add loader on home page

--- web-app/dev/scripts/controllers/home.js
+++ web-app/dev/scripts/controllers/home.js
@@ -4,11 +4,30 @@
 angular.module('yoApp')
     .controller('homeCtrl', ['$scope', 'HomeService', function ($scope, HomeService) {
 
+
         jQuery(function () {
+
             var options = {
                 types: ['(cities)']
             };
-            $("#city").geocomplete(options);
+            $("#city").geocomplete(options).bind("geocode:result", function(event, result){
+                $scope.reqLoader=true;
+                console.log('getDetail method called');
+                $scope.WILocalionResult = null;
+                $scope.WIWeatherResult = null;
+                HomeService.getDetails(function (result) {
+                    if (result) {
+                        $scope.WILocalionResult = result;
+                        $scope.getWeather(result);
+
+                    }
+                    else {
+                        $scope.showInfo = true;
+                        $scope.$apply();
+                    }
+                });
+
+            });;
         });
 
         HomeService.getMostSearchPlaceDetails(function (data) {
@@ -25,14 +44,18 @@ angular.module('yoApp')
         });
         /*recieving location information and then weather information to show weather information on home page*/
         $scope.getDetails = function () {
+            $scope.reqLoader=true;
+            console.log('getDetail method called');
             $scope.WILocalionResult = null;
             $scope.WIWeatherResult = null;
+
 
             $scope.showInfo = false;
             HomeService.getDetails(function (result) {
                 if (result) {
                     $scope.WILocalionResult = result;
                     $scope.getWeather(result);
+                    $scope.reqLoader=false;
                 }
                 else {
                     $scope.showInfo = true;
@@ -42,6 +65,10 @@ angular.module('yoApp')
         };
 
         $scope.getWeather = function (result) {
+            $scope.reqLoader=true;
+            console.log('getDetail method called');
+            $scope.WILocalionResult = null;
+            $scope.WIWeatherResult = null;
             var flag = false;
             $scope.mostVisitedData.forEach(function (mostVisited) {
                 if (mostVisited.geoNameId == (result.geoNameId?result.geoNameId:result.geonames[0].geonameId)) {
