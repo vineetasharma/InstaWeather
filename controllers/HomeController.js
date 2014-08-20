8fbfdc897ac62773267a24c9f0a5d30969ae42c6
vineetasharma <vineeta.sharma@intelligrape.com>
2014-08-20 12:51:28 +0530

    refectored the code of HomeController

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -14,34 +14,19 @@
 "use strict";
 
 exports.index = function (req, res) {
-    if (req.user) {
-        res.loginUser(req.user._id, req.user.username, ['user']);
-    }
-    console.log(req.user, 'user profile');
-    var user = req.checkLoggedIn();
-    console.log(user,'user');
-    if(user){
-        console.log('if part');
-        User.findOne({_id: user._id}, function (err, data) {
-            if (err) {
-                res.render('index', {user: null, picURL:null});
-            }
-            else if (data) {
-                console.log(data,'data chggv vjhjh');
-                res.render('index', {user: data.username, picURL:data.profilePicUrl});
+    HomeService.getUserSessionDetails(req,res)
+        .on(EventName.ERROR, function (err) {
+            log.error(err);
+            res.render('index', {user: {name:null,picURL:null}});
+        })
+        .on(EventName.NOT_FOUND, function (user) {
+           log.info(user,'user');
+            res.render('index', {user: {name:(user ? user.username : null), picURL: (user ? user.profilePicUrl:null)}});
 
-            }
+        })
+        .on(EventName.DONE, function (data) {
+            res.render('index', {user: {name:data.username, picURL:data.profilePicUrl}});
         });
-
-    }
-    else{
-        console.log(req.user);
-        res.render('index', {user: (user ? user.name : (req.user ? req.user.username : null)), picURL: (user ? user.profilePicUrl : (req.user ? req.user.profilePicUrl : null))});
-        console.log('else part of request');
-
-
-    }
-
 };
 
 var EventName = require("../src/enum/EventName");
@@ -54,7 +39,6 @@ exports.sendMail = function (req, res) {
             res.sendErrorAPIResponse(err.message, HttpStatusCode.SERVER_ERROR);
         })
         .on(EventName.DONE, function (data) {
-            log.info(data, 'in contact controller');
             res.sendSuccessAPIResponse(data, HttpStatusCode.SUCCESS_READ_OPERATION_PERFORMED);
         });
 };
--- services/HomeService.js
+++ services/HomeService.js
@@ -7,7 +7,6 @@ exports.sendMailService = function (mailData) {
   var emitter = this;
   var file=fs.readFileSync(_process.cwd()+'/web-app/dev/views/mailMessage.ejs',"utf8");
   var html=_ejs.render(file,mailData);
-  console.log(mailData, 'maildata');
   var transport = _nodemailer.createTransport( {
     service: _config.mailService.service,
     auth: {
@@ -24,7 +23,7 @@ exports.sendMailService = function (mailData) {
   transport.sendMail(options, function (err,res) {
 
     if (err) {
-      log.info('in User service  err ',err.message);
+      log.info('in Home service  err ',err.message);
       emitter.emit(EventName.ERROR, err);
     }
     else {
@@ -32,4 +31,27 @@ exports.sendMailService = function (mailData) {
     }
 
   });
+}.toEmitter();
+exports.getUserSessionDetails=function (req,res) {
+  var emitter = this;
+    if (req.user) {
+        res.loginUser(req.user._id, req.user.username, ['user']);
+    }
+    var user = req.checkLoggedIn();
+    if(user){
+        User.findOne({_id: user._id}, function (err, data) {
+            if (err) {
+                emitter.emit(EventName.ERROR, err);
+            }
+            else if (data) {
+                emitter.emit(EventName.DONE, data);
+
+            }
+        });
+
+    }
+    else{
+        emitter.emit(EventName.NOT_FOUND, req.user);
+    }
+
 }.toEmitter();
