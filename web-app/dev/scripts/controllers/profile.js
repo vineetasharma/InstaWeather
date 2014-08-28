81e99ff3b23f9cc43c15618f25526f1726f39c8d
sandeepchhapola <schhapola0001@gmail.com>
2014-08-28 13:00:23 +0530

    Validation on Hometown and state is implemented and some validation bugs are fixed

--- web-app/dev/scripts/controllers/profile.js
+++ web-app/dev/scripts/controllers/profile.js
@@ -13,18 +13,46 @@ angular.module('yoApp')
         $scope.getProfileData = function (userId) {
             if (userId) {
                 HomeService.getProfileData(userId, function (userData) {
-                    $scope.currentUser = userData;
+
+                    $scope.profilePicUrl=userData.profilePicUrl;
+
+                    $scope.Name = userData.username ? userData.username : '';
+
+                    $scope.Email = userData.email ? userData.email : '';
+
+                    $scope.About = userData.About ? userData.About : '';
+
+                    $scope.Birthday = userData.profileData.Birthday ? userData.profileData.Birthday : '';
+
+                    $scope.Gender = userData.profileData.Gender ? userData.profileData.Gender : '';
+
+                    $scope.Mobile = ''+(userData.profileData.Mobile ? userData.profileData.Mobile:'');
+
+                    $scope.lastSearchedPlace = userData.lastSearchedLocation.fullName ? userData.lastSearchedLocation.fullName : '';
+
+                    $scope.Hometown = userData.Address.Hometown ? userData.Address.Hometown : '';
+
+                    $scope.City = userData.Address.City ? userData.Address.City : '';
+
+                    $scope.State = userData.Address.State ? userData.Address.State : '';
+
+                    $scope.Country = userData.Address.Country ? userData.Address.Country : '';
+
+                    $scope.Pin = ''+(userData.Address.pin ? userData.Address.pin : '');
+
                 });
             }
             else {
                 $window.location.href = '#/home';
             }
         };
+
         $scope.getProfileData($("#profileData").val());
 
         $scope.showEditProfileForm = function () {
             $scope.isEditable = true;
         };
+
         $scope.hideEditProfileForm = function () {
             $scope.isEditable = false;
         };
@@ -34,12 +62,13 @@ angular.module('yoApp')
             if (input.files && input.files[0]) {
                 var reader = new FileReader();
                 reader.onload = function (e) {
-                   $scope.uploadPicURL=e.target.result;
+                    $scope.uploadPicURL = e.target.result;
                     $scope.$apply();
                 };
                 reader.readAsDataURL(input.files[0]);
             }
         };
+
         $scope.clearUploadedPic = function () {
             $scope.isuploadPic = false;
             jQuery('#uploadPic').val('');
